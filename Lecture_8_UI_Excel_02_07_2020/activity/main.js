870651d5f9a6952de61a6f80bf6a024c2150887f
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-02 22:10:32 +0530

    updated with positioning

---
+++ Lecture_8_UI_Excel_02_07_2020/activity/main.js
@@ -0,0 +1,51 @@
+// npm init -y
+// npm install electron --save-dev
+// create main.js file
+// goto => package.json
+// change main, add start script
+// write code
+// to run => npm start
+const electron = require("electron");
+const app = electron.app;
+const ejs = require("ejs-electron");
+ejs.data({
+  "title": "My Excel",
+  "Heading": "My First Excel APP",
+  "rows": 100,
+  "cols": 26
+})
+
+function createWindow() {
+  let win = new electron.BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.loadFile("index.ejs").then(function () {
+    // console.log("Window opened successfully");
+    win.maximize();
+    win.webContents.openDevTools();
+  })
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
