3be4f473fe67288affe042aea125f96a2fb8bbb3
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-25 22:04:33 +0530

    added jstree logic

--- Lecture-13_UI_VS/index.js
+++ Lecture-13_UI_VS/index.js
@@ -1,7 +1,6 @@
 const $ = require("jquery");
 const path = require("path");
 const fs = require("fs");
-const { dir } = require("console");
 require("jstree");
 $(document).ready(function () {
 
@@ -20,31 +19,39 @@ $(document).ready(function () {
     // i have path
     let pPath = process.cwd();
     let name = path.basename(pPath);
-
     let data = [{
         id: pPath,
         parent: "#",
         text: name
     }]
     let childArr = createData(pPath);
-
     data = [...data, ...childArr];
     $("#tree").jstree({
         "core": {
+            "check_callback" : true,
             "data": data
         },
-    })
+    }).on("select_node.jstree",
+        function (e, data) {
 
+            let cNodePath = data.node.id;
+            let cArr = createData(cNodePath);
+            for (let i = 0; i < cArr.length; i++) {
+                console.log(cArr[i]);
+                $('#tree').jstree().create_node(cNodePath, cArr[i], "last");
+
+            }
+        })
 })
 // { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" }
-function createData(parent) {
-    let childrens = fs.readdirSync(parent);
+function createData(parentPath) {
+    let childrens = fs.readdirSync(parentPath);
     let cdata = [];
     for (let i = 0; i < childrens.length; i++) {
-        let id = path.join(parent, childrens[i]);
+        let cPath = path.join(parentPath, childrens[i]);
         let obj = {
-            id: id,
-            parent:parent,
+            id: cPath,
+            parent: parentPath,
             text: childrens[i]
         };
         cdata.push(obj);
