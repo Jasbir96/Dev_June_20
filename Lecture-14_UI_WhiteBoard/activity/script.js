ffbc54507a51ed1afbf0394561e28a43cdb4e58c
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-08 19:50:24 +0530

    BP-3 added pencil behaviour

---
+++ Lecture-14_UI_WhiteBoard/activity/script.js
@@ -0,0 +1,26 @@
+
+let ispendown = false;
+board.addEventListener("mousedown", function (e) {
+    // path start
+    let x = e.clientX;
+    let y = e.clientY;
+    //  move to
+    ctx.moveTo(x, y);
+    ispendown = true;
+})
+board.addEventListener("mousemove", function (e) {
+    //  lineto 
+    let x = e.clientX;
+    let y = e.clientY;
+    if (ispendown == true) {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+    // repeat
+})
+
+board.addEventListener("mouseup", function (e) {
+    // mouse up
+    ispendown = false;
+
+})
