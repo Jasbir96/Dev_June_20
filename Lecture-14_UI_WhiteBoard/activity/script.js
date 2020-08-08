693df4094700792aef4172569c293132c8b34382
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-08 20:30:52 +0530

    BP-4 added resize n postioning logic

--- Lecture-14_UI_WhiteBoard/activity/script.js
+++ Lecture-14_UI_WhiteBoard/activity/script.js
@@ -4,6 +4,8 @@ board.addEventListener("mousedown", function (e) {
     // path start
     let x = e.clientX;
     let y = e.clientY;
+   let top= getPosition();
+   y = y - top;
     //  move to
     ctx.moveTo(x, y);
     ispendown = true;
@@ -12,6 +14,8 @@ board.addEventListener("mousemove", function (e) {
     //  lineto 
     let x = e.clientX;
     let y = e.clientY;
+    let top= getPosition();
+    y = y - top;
     if (ispendown == true) {
         ctx.lineTo(x, y);
         ctx.stroke();
@@ -24,3 +28,8 @@ board.addEventListener("mouseup", function (e) {
     ispendown = false;
 
 })
+
+function getPosition() {
+    let { top } = board.getBoundingClientRect();
+    return top;
+}
