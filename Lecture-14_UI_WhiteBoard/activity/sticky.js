74232d59c850b2a514e7b03c73b7da2d97c4c5aa
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-14 20:18:13 +0530

    C-3 B-2 made sticky dynamic

--- Lecture-14_UI_WhiteBoard/activity/sticky.js
+++ Lecture-14_UI_WhiteBoard/activity/sticky.js
@@ -29,4 +29,49 @@ function createSticky() {
 
     // append it body
     document.body.appendChild(stickyPad);
+    let initialX = null;
+    let initialY = null;
+    let isStickyDown = false;
+    navBar.addEventListener("mousedown", function (e) {
+        // initial point
+        initialX = e.clientX
+        initialY = e.clientY
+        isStickyDown = true;
+    })
+    board.addEventListener("mousemove", function (e) {
+        if (isStickyDown == true) {
+            // final point 
+            let finalX = e.clientX;
+            let finalY = e.clientY;
+            //  distance
+            let dx = finalX - initialX;
+            let dy = finalY - initialY;
+            //  move sticky
+            let { top, left } = stickyPad.getBoundingClientRect()
+            // stickyPad.style.top=10+"px";
+            stickyPad.style.top = top + dy + "px";
+            stickyPad.style.left = left + dx + "px";
+            initialX = finalX;
+            initialY = finalY;
+        }
+    })
+
+    window.addEventListener("mouseup", function () {
+        isStickyDown = false;
+    })
+
+    close.addEventListener("click", function () {
+        stickyPad.remove();
+    })
+    let flag = true;
+    minimize.addEventListener("click", function () {
+
+        if (flag == true) {
+            container.style.display = "none";
+        } else {
+            container.style.display = "block";
+
+        }
+        flag=!flag;
+    })
 }
