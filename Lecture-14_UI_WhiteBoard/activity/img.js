38c07a865b9f12fca766a48de15d255e32e4228e
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-16 18:56:02 +0530

    C-3 B-2 Image upload

--- Lecture-14_UI_WhiteBoard/activity/img.js
+++ Lecture-14_UI_WhiteBoard/activity/img.js
@@ -1,16 +1,16 @@
 let upload = document.querySelector("#upload");
-upload.addEventListener("click", function (e) {
+upload.addEventListener("change", function (e) {
     let uInp = document.querySelector("input[type='file']");
-    uInp.click();
     // any image is selected
-    uInp.addEventListener("change", function () {
-        let container = createBox();
-        let file = uInp.files[0];
-        let img = document.createElement("img");
-        img.src = URL.createObjectURL(file);
-        img.setAttribute("class", "upload-img")
-        container.appendChild(img);
-    })
+    // let changed = false;
+    let container = createBox();
+    let file = uInp.files[0];
+    let img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    img.setAttribute("class", "upload-img");
+    container.appendChild(img);
+//  remove
+    uInp.value = null;
     // const reader = new FileReader();
     // reader.readAsDataURL(upload.files[0]);    
     //  using file Reader
@@ -18,3 +18,4 @@ upload.addEventListener("click", function (e) {
     //     img.src = reader.result;
     // }
 })
+
