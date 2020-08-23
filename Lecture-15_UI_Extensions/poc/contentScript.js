0443b2aac1651d23feb7b5250ce6ce7cf8134068
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-23 15:49:27 +0530

    C-1 B-4 added messaging

--- Lecture-15_UI_Extensions/poc/contentScript.js
+++ Lecture-15_UI_Extensions/poc/contentScript.js
@@ -1,20 +1,30 @@
 
 
 // console.log("I ran")
-let imgArr = document.querySelectorAll("img");
-let images = ["images/img-1.jpg",
-    "images/img-2.jpg",
-    "images/img-3.jpg",
-    "images/img-4.jpg",
-    "images/img-5.jpg",
-];
+function changeImages() {
+    let imgArr = document.querySelectorAll("img");
+    let images = ["images/img-1.jpg",
+        "images/img-2.jpg",
+        "images/img-3.jpg",
+        "images/img-4.jpg",
+        "images/img-5.jpg",
+    ];
 
-for (let i = 0; i < imgArr.length; i++) {
-    // console.log(chrome.extension.getURL(images[0]))
-    let idx = Math.floor(Math.random() * images.length);
-    let url = chrome.extension.getURL(images[idx])
-    console.log(url);
-    imgArr[i].src = url;
+    for (let i = 0; i < imgArr.length; i++) {
+        // console.log(chrome.extension.getURL(images[0]))
+        let idx = Math.floor(Math.random() * images.length);
+        let url = chrome.extension.getURL(images[idx])
+        console.log(url);
+        imgArr[i].src = url;
 
+    }
 }
 
+// response
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        // console.log(request)
+        sendResponse("Hello from content");
+        // console.log(sender);
+        changeImages();
+    });
--- Lecture-15_UI_Extensions/poc/popup.js
+++ Lecture-15_UI_Extensions/poc/popup.js
@@ -1,6 +1,13 @@
 console.log("Inside popup");
 let btn = document.querySelector(".click");
 btn.addEventListener("click", function () {
-console.log("Popup btn was clicked")
-console.log("yha se content change ")
+    console.log("Popup btn was clicked")
+    console.log("yha se content change ");
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log(tabs)
+        // to send message
+        chrome.tabs.sendMessage(tabs[0].id, "hello from popup",function(response){
+            console.log(response);
+        });
+    })
 })
