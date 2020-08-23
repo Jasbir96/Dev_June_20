b87b89d5c76801014f7a8eb87f42c796f44f147a
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-23 18:20:40 +0530

    updated references for openbpard and ext

--- Lecture-15_UI_Extensions/poc/background.js
+++ Lecture-15_UI_Extensions/poc/background.js
@@ -2,6 +2,7 @@ console.log("Inside background");
 // chrome.tabs.onCreated.addListener(function (tab) {
 //     console.log(tab);
 // })
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (tab.url.includes("facebook")) {
         chrome.tabs.remove(tabId, function () {
