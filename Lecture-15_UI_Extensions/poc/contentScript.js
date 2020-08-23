7e139853881d8c1ef147f8af210408fbc5ee73ec
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-23 15:05:38 +0530

    C-1 B-3 added popup.js

--- Lecture-15_UI_Extensions/poc/contentScript.js
+++ Lecture-15_UI_Extensions/poc/contentScript.js
@@ -1,14 +1,14 @@
 
 
-console.log("I ran")
+// console.log("I ran")
 let imgArr = document.querySelectorAll("img");
-
 let images = ["images/img-1.jpg",
     "images/img-2.jpg",
     "images/img-3.jpg",
     "images/img-4.jpg",
     "images/img-5.jpg",
 ];
+
 for (let i = 0; i < imgArr.length; i++) {
     // console.log(chrome.extension.getURL(images[0]))
     let idx = Math.floor(Math.random() * images.length);
---
+++ Lecture-15_UI_Extensions/poc/popup.js
@@ -0,0 +1,6 @@
+console.log("Inside popup");
+let btn = document.querySelector(".click");
+btn.addEventListener("click", function () {
+console.log("Popup btn was clicked")
+console.log("yha se content change ")
+})
