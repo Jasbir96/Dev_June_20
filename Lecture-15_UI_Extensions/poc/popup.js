5f1c666206fbc7ceeec0e98b777bd11f3badfebc
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-27 19:00:10 +0530

    C-1 B-1 added popup.js

--- Lecture-15_UI_Extensions/poc/popup.js
+++ Lecture-15_UI_Extensions/poc/popup.js
@@ -3,6 +3,7 @@ let btn = document.querySelector(".click");
 btn.addEventListener("click", function () {
     console.log("Popup btn was clicked")
     console.log("yha se content change ");
+    // popup 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         console.log(tabs)
         // to send message
