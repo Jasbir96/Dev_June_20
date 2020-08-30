f1ab897f01aa637e8fc7289ccf30e32891de50ce
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-30 23:33:29 +0530

    C-1 B-3 added crud for api

--- Lecture_16_Full_Stack_Insta/raw/poc/app.js
+++ Lecture_16_Full_Stack_Insta/raw/poc/app.js
@@ -1,40 +1,32 @@
 //  api making framework
 const express = require("express");
-const users = require("./db/user.json");
+let userDB = require("./db/user.json");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const path = require("path");
 // const { response } = require("express");
 const app = express();
-// get ,post ,patch ,delete => express methods
-// 127.0.0.1:3000=> localhost:3000/home
-// get All=> admin
-// get=> particular a user
-// post => create  a user 
-// update => update a user
-// delete a user
-// name,password,handle,image_url,bio,uid,email
-// for accepting data in req.body
+// // for accepting data in req.body
 app.use(express.json());
-// create 
-app.post("/user", (req, res) => {
-    let user = req.body;
-    console.log(user);
-    user.uid = uuidv4();
-    users.push(user);
 
-    // saved to disk
-    fs.writeFileSync("./db/user.json", JSON.stringify(users));
-    // res
+// get => some changing parameter 
+// getOne
+// npm i uuid
+// npm i nodemon --save-dev
+const { v4: uuidv4 } = require('uuid');
+// get All userDB
+app.get("/user", (req, res) => {
+    // req paramatere -> user id
+    // console.log(req.params);
     res.status(201).json({
         status: "success",
-        user: req.body
+        userDB: userDB
     })
 })
-// get => some changing parameter 
+// get One
 app.get("/user/:uid", (req, res) => {
     // req paramatere -> user id
     let cUid = req.params.uid;
-    let userArr = users.filter((user) => {
+    let userArr = userDB.filter((user) => {
         return user.uid == cUid;
     });
     console.log(req.params);
@@ -43,11 +35,70 @@ app.get("/user/:uid", (req, res) => {
         user: userArr.length == 0 ? "no user" : userArr[0]
     })
 })
+// create 
+app.post("/user", (req, res) => {
+    let user = req.body;
+    console.log(user);
+    user.uid = uuidv4();
+    userDB.push(user);
 
+    // saved to disk
+    fs.writeFileSync(path.join(__dirname, "/db/user.json"), JSON.stringify(userDB));
+    // res
+    res.status(201).json({
+        status: "success",
+        user: req.body
+    })
+})
+// // updated => key search 
+app.patch("/user/:uid", (req, res) => {
+    let user = getUserById(req.params.uid);
+    let toBeUpdatedObj = req.body;
+    // user , obj
+    // user.something
+    for (let key in toBeUpdatedObj) {
+        console.log(key);
+        user[key] = toBeUpdatedObj[key];
+    }
+    fs.writeFileSync(path.join(__dirname, "/db/user.json"), JSON.stringify(userDB));
+    res.status(200).json({
+        status: "success",
+        user: user
+    })
+})
 // delete => filter
-// updated => key search 
-// users.splice(idx,1);
+app.delete("/user/:uid", (req, res) => {
+    let cid = req.params.uid;
+    console.log(userDB.length);
+    userDB = userDB.filter((user) => { return user.uid != cid; })
+    fs.writeFileSync(path.join(__dirname, "/db/user.json"), JSON.stringify(userDB));
+    res.status(200).json({
+        status: "success",
+        userDB,
+        length: userDB.length
+    })
+
+})
+
+
+// userDB.splice(idx,1);
+// get ,post ,patch ,delete => express methods
+// 127.0.0.1:3000=> localhost:3000/home
+// get All=> admin
+// get=> particular a user
+// post => create  a user 
+// update => update a user
+// delete a user
+// name,password,handle,image_url,bio,uid,email
 
 app.listen(3000, () => {
     console.log("Server started at port 3000");
 })
+function getUserById(cUid) {
+
+
+    let userArr = userDB.filter((user) => {
+        return user.uid == cUid;
+    });
+    return userArr[0];
+}
---
+++ Lecture_16_Full_Stack_Insta/raw/poc/facts/file.js
@@ -0,0 +1,8 @@
+// global object that  is synonymous to window of browser
+// console.log(global);
+// current process
+console.log(process);
+// current directory path
+// console.log(__dirname);
+// current file
+// console.log(__filename);
